de260d0cb29ea951c2323a1275d58ff0455f7a64
gottsch <mark.gottschling@gmail.com>
2023-04-27 09:19:50 -0400

    updates

--- src/main/java/mod/gottsch/fabric/dungeonblocks/config/ID.java
+++ src/main/java/mod/gottsch/fabric/dungeonblocks/config/ID.java
@@ -8,4 +8,20 @@ public class ID {
     public static final String SMOOTH_STONE_FACADE = "smooth_stone_facade_block";
     public static final String COBBLESTONE_FACADE = "cobblestone_facade_block";
     public static final String MOSSY_COBBLESTONE_FACADE = "mossy_cobblestone_facade_block";
+    public static final String BRICKS_FACADE = "bricks_facade_block";
+    public static final String STONE_BRICKS_FACADE = "stone_bricks_facade_block";
+    public static final String MOSSY_STONE_BRICKS_FACADE = "mossy_stone_bricks_facade_block";
+    public static final String CRACKED_STONE_BRICKS_FACADE = "cracked_stone_bricks_facade_block";
+    public static final String CHISELED_STONE_BRICKS_FACADE = "chiseled_stone_bricks_facade_block";
+    public static final String OBSIDIAN_FACADE = "obsidian_facade_block";
+
+    public static final String SANDSTONE_FACADE = "sandstone_facade_block";
+    public static final String SMOOTH_SANDSTONE_FACADE = "smooth_sandstone_facade_block";
+    public static final String CHISELED_SANDSTONE_FACADE = "chiseled_sandstone_facade_block";
+    public static final String CUT_SANDSTONE_FACADE = "cut_sandstone_facade_block";
+    public static final String RED_SANDSTONE_FACADE = "red_sandstone_facade_block";
+    public static final String SMOOTH_RED_SANDSTONE_FACADE = "smooth_red_sandstone_facade_block";
+    public static final String CHISELED_RED_SANDSTONE_FACADE = "chiseled_red_sandstone_facade_block";
+    public static final String CUT_RED_SANDSTONE_FACADE = "cut_red_sandstone_facade_block";
+
 }
